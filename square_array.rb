2ec1d06a767e8e6f179ba6550e20aab772ebf380
Panela Leung <leungpanela@gmail.com>
2019-08-23 22:39:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   # your code here
   new_array= Array.new
+  length = array.length
+  while length > 0
   array.each do |num|
     operation = num ** num
     new_array << operation
